da2608318d935ebfad8033249a26741286c891e1
Alisson Vargas <contato@alissonsv.com>
2024-07-04 21:43:16 -0300

    feat: finish blog post layout configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), icon()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+    },
+  },
 });
